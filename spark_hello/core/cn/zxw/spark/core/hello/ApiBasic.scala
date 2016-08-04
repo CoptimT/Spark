230b5e6b31d5b7f9cbbb11bdc37274664429bc34
CoptimT <zxw644@163.com>
2016-08-04 18:19:29 +0800

    update spark core

---
+++ spark_hello/core/cn/zxw/spark/core/hello/ApiBasic.scala
@@ -0,0 +1,43 @@
+package cn.zxw.spark.core.hello
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+import org.apache.hadoop.io.LongWritable
+import org.apache.hadoop.io.Text
+import org.apache.hadoop.mapred.JobConf
+import org.apache.hadoop.mapred.TextInputFormat
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.mapreduce.lib.input.{TextInputFormat=>NewTextInputFormat}
+
+
+/**
+ * @author lx
+ */
+object ApiBasic {
+  def main(args: Array[String]): Unit = {
+    val master = args(0)
+    val conf = new SparkConf().setMaster(master).setAppName("ApiBasic")
+    val sc = new SparkContext(conf)
+    
+    val tf1 = sc.textFile("/my/directory")
+    val tf2 = sc.textFile("/my/directory/*.txt")
+    val tf3 = sc.textFile("/my/directory/*.gz")
+    
+    val wtf = sc.wholeTextFiles("data.txt") //(filename, content) pairs
+    
+    val sf1 = sc.sequenceFile[LongWritable,Text]("data.txt")
+    val sf2 = sc.sequenceFile("data.txt",classOf[LongWritable],classOf[Text])
+    
+    val jobConf = new JobConf()
+    val rdd1 = sc.hadoopRDD(jobConf, classOf[TextInputFormat], classOf[LongWritable],classOf[Text])
+    val rdd2 = sc.hadoopFile("data.txt", classOf[TextInputFormat], classOf[LongWritable],classOf[Text])
+    val config = new Configuration()
+    val rdd3 = sc.newAPIHadoopRDD(config, classOf[NewTextInputFormat], classOf[LongWritable],classOf[Text])
+    val rdd4 = sc.newAPIHadoopFile("data.txt", classOf[NewTextInputFormat], classOf[LongWritable],classOf[Text], config)
+    
+    rdd4.saveAsTextFile("")
+    rdd4.saveAsObjectFile("")
+    
+    
+  }
+}
--- spark_hello/core/cn/zxw/spark/core/hello/WordCount.scala
+++ spark_hello/core/cn/zxw/spark/core/hello/WordCount.scala
@@ -6,13 +6,29 @@ import org.apache.spark.SparkContext
 object WordCount {
   
   def main(args: Array[String]): Unit = {
-    val file = args(0)
-    var conf=new SparkConf().setAppName("Word Count")
-    var sc=new SparkContext(conf)
-    var lines=sc.textFile(file)
-    var words=lines.flatMap(_.split("\\s+"))
-    var count=words.countByValue()
-    println(count)
+    //testMaster(args)
+    testLocal()
   }
   
+  def testLocal(){
+    val conf = new SparkConf().setMaster("local").setAppName("test")
+    var sc = new SparkContext(conf)
+    var rdd = sc.textFile("file\\my\\wordcount.txt", 1)
+    var words = rdd.flatMap{line => line.split("\\s+")}.map(word => (word,1)).reduceByKey(_+_)
+    println(words.toDebugString)
+    words.foreach(f => println(f._1+" = "+f._2))
+    sc.stop()
+  }
+  
+  def testMaster(args: Array[String]): Unit = {
+    val master = args(0)
+    val file = args(1)
+    val conf = new SparkConf().setMaster(master).setAppName("ApiExample")
+    val sc = new SparkContext(conf)
+    var lines = sc.textFile(file)
+    var words = lines.flatMap(_.split("\\s+"))
+    var count = words.countByValue()
+    println(count)
+    sc.stop()
+  }
 }
