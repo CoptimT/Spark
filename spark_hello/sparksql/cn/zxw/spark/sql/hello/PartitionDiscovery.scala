7d7c29d8fd6892aba9f3158cec935a03724c6954
CoptimT <zxw644@163.com>
2016-08-08 15:47:49 +0800

    update spark code

--- spark_hello/sparksql/cn/zxw/spark/sql/hello/PartitionDiscovery.scala
+++ spark_hello/sparksql/cn/zxw/spark/sql/hello/PartitionDiscovery.scala
@@ -4,14 +4,14 @@ import org.apache.spark.SparkContext
 
 object PartitionDiscovery {
   def main(args: Array[String]): Unit = {
-    val sc = new SparkContext("local","SchemaMerging")
+    val sc = new SparkContext("local","PartitionDiscovery")
     val sqlContext = new org.apache.spark.sql.SQLContext(sc)
     import sqlContext.implicits._
     
     val r1=sqlContext.read.parquet("hdfs://10.10.25.14:8020/test/spark/tb_bsdk/")
-    ///test/spark/tb_bsdk/country=china/ide=eclipse
+    // /test/spark/tb_bsdk/country=china/ide=eclipse
     r1.show()
-    //| id|    date|channel_id|dh_id|users|total|country|    ide
+    // |id|date|channel_id|dh_id|users|total|country|ide
     
   }
 }
