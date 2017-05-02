262446a68d0f973a3e5784017fab1c3039a1fa97
zhangxiangwei <zhangxiangwei@yixia.com>
2017-05-02 19:02:11 +0800

    spark streaming example

--- spark_streaming_1.6.3/src/main/scala/cn/zxw/spark/streaming/api/WindowWordCount.scala
+++ spark_streaming_1.6.3/src/main/scala/cn/zxw/spark/streaming/api/WindowWordCount.scala
@@ -11,15 +11,25 @@ object WindowWordCount {
   def main(args: Array[String]): Unit = {
     val sparkConf = new SparkConf().setMaster("local[3]").setAppName("WindowWordCount")
     val ssc = new StreamingContext(sparkConf,Seconds(5))
+    ssc.checkpoint("checkpoint")
     
     val lines = ssc.socketTextStream("localhost", 9999, StorageLevel.MEMORY_ONLY)
-    
     val words = lines.flatMap { x => x.split("\\s") }
-    val pairs = words.map { x => (x,1) }
-    val wordcount = pairs.reduceByKeyAndWindow((a:Int,b:Int) => a+b,Seconds(20),Seconds(10))
-    
-    wordcount.print()
-    
+
+    //words.map { x => (x,1) }.reduceByKeyAndWindow((a:Int,b:Int) => a+b,Seconds(20),Seconds(10)).print()
+    words.map { x => (x,1) }.reduceByKeyAndWindow((a:Int,b:Int) => a+b,(a:Int,b:Int) => a-b,Seconds(20),Seconds(10)).print()
+
+    /**
+    words.window(Seconds(10),Seconds(5)).countByValue().print()//DStream[(String, Long)]
+
+    words.countByWindow(Seconds(10),Seconds(5)).print()//DStream[Long]
+    words.countByValueAndWindow(Seconds(10), Seconds(5)).print()//DStream[(String, Long)]
+
+    words.reduceByWindow((a:String,b:String) => a+","+b, Seconds(10), Seconds(5))//DStream[T]
+    val wc = words.map((_,1))
+    wc.reduceByKeyAndWindow((a:Int,b:Int) => a+b,Seconds(20),Seconds(10))//DStream[(K, V)]
+    wc.reduceByKeyAndWindow((a:Int,b:Int) => a+b,(a:Int,b:Int) => a-b,Seconds(20),Seconds(10))//DStream[(K, V)]
+    */
     ssc.start()
     ssc.awaitTermination()
   }
