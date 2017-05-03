76a952362de799ece233cb2b24f358ccd7b87d91
zhangxiangwei <zhangxiangwei@yixia.com>
2017-05-03 16:12:24 +0800

    spark streaming example

---
+++ spark_streaming_1.6.3/src/main/scala/cn/zxw/spark/streaming/kafka/UserClickCountAnalytics.scala
@@ -0,0 +1,64 @@
+package cn.zxw.spark.streaming.kafka
+
+import cn.zxw.spark.streaming.util.{KafkaRedisProperties, RedisClient}
+import com.alibaba.fastjson.JSON
+import kafka.serializer.StringDecoder
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+import org.apache.spark.streaming.kafka.KafkaUtils
+
+object UserClickCountAnalytics {
+  def main(args: Array[String]): Unit = {
+    var masterUrl = "local[1]"
+    if (args.length > 0) {
+      masterUrl = args(0)
+    }
+
+    // Create a StreamingContext with the given master URL
+    val conf = new SparkConf().setMaster(masterUrl).setAppName("UserClickCountStat")
+    val ssc = new StreamingContext(conf, Seconds(5))
+
+    // Kafka configurations
+    val topics = KafkaRedisProperties.KAFKA_USER_TOPIC.split("\\,").toSet
+    println(s"Topics: ${topics}.")
+
+    val brokers = KafkaRedisProperties.KAFKA_ADDR
+    val kafkaParams = Map[String, String](
+      "metadata.broker.list" -> brokers,
+      "serializer.class" -> "kafka.serializer.StringEncoder"
+    )
+
+    val clickHashKey = "app::users::click"
+
+    // Create a direct stream
+    val kafkaStream = KafkaUtils
+        .createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topics)
+
+    val events = kafkaStream.flatMap(line => {
+      println(s"Line ${line}.")
+      val data = JSON.parseObject(line._2)
+      Some(data)
+    })
+
+    // Compute user click times
+    val userClicks = events.map(x => (x.getString("uid"), x.getLong("click_count"))).reduceByKey(_ + _)
+    userClicks.foreachRDD(rdd => {
+      rdd.foreachPartition(partitionOfRecords => {
+        val jedis = RedisClient.pool.getResource
+        partitionOfRecords.foreach(pair => {
+          try {
+            val uid = pair._1
+            val clickCount = pair._2
+            jedis.hincrBy(clickHashKey, uid, clickCount)
+            println(s"Update uid ${uid} to ${clickCount}.")
+          } catch {
+            case e: Exception => println("error:" + e)
+          }
+        })
+      })
+    })
+
+    ssc.start()
+    ssc.awaitTermination()
+  }
+}
