f2b0888cb1114f8075f5ca425f3fefe38cd330c7
zhangxiangwei <zhangxiangwei@yixia.com>
2017-08-16 17:40:36 +0800

    spark example

--- spark_2.1.1/src/main/scala/cn/zxw/spark/sql/BuildInDatasourceJson.scala
+++ spark_2.1.1/src/main/scala/cn/zxw/spark/sql/BuildInDatasourceJson.scala
@@ -9,6 +9,7 @@ object BuildInDatasourceJson {
   def main(args: Array[String]): Unit = {
     val spark = SparkSession
         .builder()
+        .master("local")
         .appName("Spark SQL basic example")
         .config("spark.some.config.option", "some-value")
         .getOrCreate()
@@ -16,9 +17,12 @@ object BuildInDatasourceJson {
     // For implicit conversions like converting RDDs to DataFrames
     import spark.implicits._
 
+    val path = "spark_2.1.1/src/main/resources/examples/"
+    val people = spark.read.json(path+"people.json")
+    people.printSchema()
+    people.show()
 
-
-
+    spark.stop()
 
   }
 
